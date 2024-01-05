52aa4a9ed6f8f1354aa2b49493e4700d7918955d
ThachSangW3G <thachsang2202@gmail.com>
2024-01-06 01:12:54 +0700

    xong bang

--- lib/screens/tabs/contract.tab.dart
+++ lib/screens/tabs/contract.tab.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
+import 'package:quanlynhanvien/constants/app_colors.dart';
+import 'package:quanlynhanvien/models/hopdonglaodong.model.dart';
 
 class ConstactTab extends StatelessWidget {
   const ConstactTab({super.key});
@@ -6,10 +11,194 @@ class ConstactTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            color: Colors.blueAccent,
-            child: const Center(
-              child: Text("ConstactTab"),
-            )));
+        body: Column(
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: 10.0,
+            ),
+            SizedBox(
+              width: 300,
+              height: 40,
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(20)),
+                  prefixIcon: SvgPicture.asset(
+                    '/icon_svg/search.svg',
+                    fit: BoxFit.scaleDown,
+                    color: Colors.black,
+                  ),
+                  hintText: 'Search',
+                  hintStyle: const TextStyle(
+                    color: AppColors.greyBombay,
+                    fontFamily: 'SF Pro Display',
+                  ),
+                  filled: true,
+                  fillColor: AppColors.greyIron,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            InkWell(
+              child: Container(
+                width: 100,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: AppColors.bluePrussian,
+                    borderRadius: BorderRadius.circular(15)),
+                alignment: Alignment.center,
+                child: const Text(
+                  'Thêm',
+                  style: TextStyle(
+                      fontFamily: 'CeraPro', fontSize: 16, color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            InkWell(
+              child: Container(
+                width: 100,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: AppColors.bluePrussian,
+                    borderRadius: BorderRadius.circular(15)),
+                alignment: Alignment.center,
+                child: const Text(
+                  'Gia hạn',
+                  style: TextStyle(
+                      fontFamily: 'CeraPro', fontSize: 16, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: PaginatedDataTable(
+            source: RowSource(myData: listHDLD, count: listHDLD.length),
+            rowsPerPage: 10,
+            columnSpacing: 90,
+            columns: [
+              DataColumn(
+                  label: const Text(
+                    "STT",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+                  ),
+                  onSort: (columnIndex, ascending) {}),
+              DataColumn(
+                  label: const Text(
+                    "Mã HĐLĐ",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+                  ),
+                  onSort: (columnIndex, ascending) {}),
+              DataColumn(
+                  label: const Text(
+                    "Mã Nhân viên",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+                  ),
+                  onSort: (columnIndex, ascending) {}),
+              const DataColumn(
+                label: Text(
+                  "Ngày bắt đầu",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+                ),
+              ),
+              const DataColumn(
+                label: Text(
+                  "Ngày kết thúc",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+                ),
+              ),
+              const DataColumn(
+                label: Text(
+                  "Lương cơ bản",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+                ),
+              ),
+              const DataColumn(
+                label: Text(
+                  "Sửa",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+                ),
+              ),
+              const DataColumn(
+                label: Text(
+                  "Xóa",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
+}
+
+class RowSource extends DataTableSource {
+  var myData;
+  final count;
+  RowSource({
+    required this.myData,
+    required this.count,
+  });
+
+  @override
+  DataRow? getRow(int index) {
+    if (index < rowCount) {
+      return recentFileDataRow(myData![index]);
+    } else
+      return null;
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => count;
+
+  @override
+  int get selectedRowCount => 0;
+}
+
+DataRow recentFileDataRow(var data) {
+  DateTime dtBatDau = (data.ngayBatDau as Timestamp).toDate();
+  DateTime dtKetThuc = (data.ngayKetThuc as Timestamp).toDate();
+  final hdld = data as HopDongLaoDong;
+  return DataRow(
+    cells: [
+      DataCell(Text((listHDLD.indexOf(hdld) + 1).toString())),
+      DataCell(Text(data.maHD.toString())),
+      DataCell(Text(data.maNV.toString())),
+      DataCell(Text(DateFormat('MM/dd/yyyy').format(dtBatDau))),
+      DataCell(Text(DateFormat('MM/dd/yyyy').format(dtKetThuc))),
+      DataCell(Text(data.luongCoBan.toString())),
+      DataCell(IconButton(
+        icon: const Icon(Icons.edit),
+        onPressed: () {},
+      )),
+      DataCell(IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () {},
+      ))
+    ],
+  );
 }
